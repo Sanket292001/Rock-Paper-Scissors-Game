7884be0c2fd4d01c6d418166cf33864763ee5554
Sanket Salunke <salunkess2001@gmail.com>
2021-01-08 10:13:22 +0530

    Updates

---
+++ precache-manifest.ef75a8bc2f7adfe5f32879680d51f0b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e17bb7829c9d242ebeac2657d0e498",
+    "url": "/Rock-Paper-Scissors-Game/index.html"
+  },
+  {
+    "revision": "45cc8683c3f275ead3d3",
+    "url": "/Rock-Paper-Scissors-Game/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "820b9b8b54c9b68be87a",
+    "url": "/Rock-Paper-Scissors-Game/static/css/main.f20235ba.chunk.css"
+  },
+  {
+    "revision": "45cc8683c3f275ead3d3",
+    "url": "/Rock-Paper-Scissors-Game/static/js/2.b75965d3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Rock-Paper-Scissors-Game/static/js/2.b75965d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820b9b8b54c9b68be87a",
+    "url": "/Rock-Paper-Scissors-Game/static/js/main.e9fb2d38.chunk.js"
+  },
+  {
+    "revision": "f469a647d0f55b6646e4",
+    "url": "/Rock-Paper-Scissors-Game/static/js/runtime-main.9df8edbf.js"
+  }
+]);
